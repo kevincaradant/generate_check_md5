6132033893e37d874920ac1acce4f0e9bed2a18c
kevin <kevin.caradant@gmail.com>
2016-12-11 21:32:42 +0100

    fix: Problem to compare to files. Error of variable name

--- check-md5-files.js
+++ check-md5-files.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const sh = require('shelljs');
+const md5File  = require('md5-file');
 const argv = require('yargs').argv;
 const fs = require('fs');
 const LineByLineReader = require('line-by-line');
@@ -9,17 +9,6 @@ var recursiveReadSync = require('recursive-readdir-sync'), files;
 // Array of MD5 after to have read each line of source file and compare file.
 var sourceArray = [];
 var compareArray = [];
-
-
-// Generate Each md5 of file includes in --path
-// cmd = generate MD5 . Then with "sed", we replace each space by underscore starting by the second space
-// The first space is the separate between md5 and the name of the file. So we want to keep it.
-// After that we have the cut which cut at the first space found. We get the md5
-const generateMD5 = (file) => {
-  let cmd = `md5sum '${file}' | sed -e 's/ /_/2g' | cut -f1 -d" "`;
-  let rslt = sh.exec(cmd, {silent:true}).stdout.split('\\').join('');
-  return rslt;
-};
 
 // Analyse and get each md5 line by line for the two files ( pSource and pCompare )
 const analyseMD5 = (pSource, pCompare) => {
@@ -36,7 +25,7 @@ const analyseMD5 = (pSource, pCompare) => {
 
   lineByLineReader.on('line', function (line) {
     // Get only the md5. Otherwise we could get the name of the file with it md5. Write [0] to get the name or remove it and you will have name + md5
-    line = line.split(' ')[1]
+    line = line.split(' : ')[0]
     sourceArray.push(line);
   });
 
@@ -52,7 +41,7 @@ const analyseMD5 = (pSource, pCompare) => {
 
   lineByLineReader2.on('line', function (line) {
     // Get only the md5. Otherwise we could get the name of the file with it md5. Write [0] to get the name or remove it and you will have name + md5
-    line = line.split(' ')[1]
+    line = line.split(' : ')[0]
     compareArray.push(line);
   });
 
@@ -66,9 +55,9 @@ const containMD5 = (pSourceContain, pCompareContain) => {
   let cptErrorMD5 = 0;
 
   // For each lines in the compare file. We check if the md5 is the same that the other file.
-  for (md5 in pSourceContain) {
+  for (md5 in pCompareContain) {
     if(!pSourceContain.includes(pCompareContain[md5])){
-      console.log(`Error with: ${pSourceContain[md5]}`);
+      console.log(`Error with: ${pCompareContain[md5]}`);
       cptErrorMD5 ++;
     }
   }
@@ -83,24 +72,30 @@ const containMD5 = (pSourceContain, pCompareContain) => {
 
 
 if(argv.h || argv.help){
-  console.log('\n\nTo generate MD5 on console (only) :');
-  console.log('--path "/path/to/the/my_directory_with_files/"    or    -path "/path/to/the/my_directory_with_files/" \n');
-  console.log('To generate MD5 and write it in the file :');
-  console.log('--path "/path/to/the/my_directory_with_files/"    or    -path "/path/to/the/my_directory_with_files/"');
-  console.log('--dest "/path/to/write/file_md5_results.txt"    or    -dest "/path/to/write/file_md5_results.txt/" \n');
-  console.log('To compare md5 files :');
-  console.log('--source "/path/to/the/md5_file_source_its_the_reference.txt/"    or    -source "/path/to/the/md5_file_source_its_the_reference.txt/"');
-  console.log('--compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/"    or    -compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/" \n');
-  console.log('To generate AND compare md5 files in the same :');
-  console.log('--path "/path/to/the/my_directory_with_files/"    or    -path "/path/to/the/my_directory_with_files/"');
-  console.log('--dest "/path/to/write/file_md5_results.txt"    or    -dest "/path/to/write/file_md5_results.txt/"');
-  console.log('--source "/path/to/the/md5_file_source_its_the_reference.txt/"    or    -source "/path/to/the/md5_file_source_its_the_reference.txt/"');
-  console.log('--compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/"    or    -compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/" \n');
-  console.log('To rename files name in the file of results without space :');
-  console.log('-- nospace or -nospace')
-  console.log('Example:');
-  console.log('Before: /Folder1/my file for example.mkv a9asd1171dd83e122598af664bd3f785)');
-  console.log('After: /Folder1/my_file_for_example.mkv a9asd1171dd83e122598af664bd3f785)');
+  console.log(`
+  To generate MD5 on console (only) :
+  --path "/path/to/the/my_directory_with_files/"    or    -path "/path/to/the/my_directory_with_files/"
+
+  To generate MD5 and write it in the file :
+  --path "/path/to/the/my_directory_with_files/"    or    -path "/path/to/the/my_directory_with_files/"
+  --dest "/path/to/write/file_md5_results.txt"    or    -dest "/path/to/write/file_md5_results.txt/"
+
+  To compare md5 files :
+  --source "/path/to/the/md5_file_source_its_the_reference.txt/"    or    -source "/path/to/the/md5_file_source_its_the_reference.txt/"
+  --compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/"    or    -compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/"
+
+  To generate AND compare md5 files in the same :
+  --path "/path/to/the/my_directory_with_files/"    or    -path "/path/to/the/my_directory_with_files/"
+  --dest "/path/to/write/file_md5_results.txt"    or    -dest "/path/to/write/file_md5_results.txt/"
+  --source "/path/to/the/md5_file_source_its_the_reference.txt/"    or    -source "/path/to/the/md5_file_source_its_the_reference.txt/"
+  --compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/"    or    -compare "/path/to/the/md5_file_to_be_compared_with_source_file.txt/"
+
+  To rename files name in the file of results without space :
+  -- nospace or -nospace
+  Example:
+  Before: /Folder1/my file for example.mkv a9asd1171dd83e122598af664bd3f785)
+  After: /Folder1/my_file_for_example.mkv a9asd1171dd83e122598af664bd3f785)
+  `);
   return;
 }
 
@@ -129,6 +124,7 @@ else if (argv.path) {
         fs.unlinkSync(argv.dest);
       }
       catch(err) {
+        console.log(err);
         console.log(`The file with the path ${argv.dest} can\'t be remove.`);
       }
     }
@@ -136,9 +132,6 @@ else if (argv.path) {
     // For each file found
     files.forEach((file, index, arr) => {
       console.log(`${index+1} / ${arr.length}`);
-
-      // We call the method to generate a MD5 for the file
-      md5 = generateMD5(file);
 
       // We add _ instead of space to have a pretty display in the file
       if(argv.nospace) {
@@ -148,14 +141,15 @@ else if (argv.path) {
       // If we have a file to write the result
       if (argv.dest) {
         // We wrote in the file
-        fs.appendFileSync(argv.dest, `${file} ${md5}`);
+        fs.appendFileSync(argv.dest, `${md5File.sync(file)} : ${file}\n`);
       } else{
         // We wrote in the console only
-        console.log(`${file} ${md5}`);
+        console.log(`${md5} : ${file} `);
       }
     });
     console.log("Get every MD5 done !");
   } catch(err){
+    console.log(err);
     console.log('Path wrong for --path. No files found');
   }
 
