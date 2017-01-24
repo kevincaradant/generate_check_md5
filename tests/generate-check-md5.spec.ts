0483421a7dd4ab1f489ce9c8cb2089db3ddbd8cd
Kévin <kevin.caradant@gmail.com>
2017-01-24 19:14:43 +0100

    Fix the coverage + remove useless unit tests

--- tests/generate-check-md5.spec.ts
+++
@@ -1,9 +0,0 @@
-  const test = require('tape');
-  import * as checks from '../src/checks';
-(() => {
-  test('Date is a function', (t: any) => {
-      t.plan(1);
-      t.false(checks.checkHelp(true, false));
-      t.end();
-  });
-})();
